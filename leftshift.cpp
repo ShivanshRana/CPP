da4f65ddef5a1991314fb82617d35d21f067f1cb
ShivanshRana <shivanshrana23@gmail.com>
2023-11-09 20:43:51 +0530

    leftshiftby1inarray

---
+++ leftshift.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+     cin>>n;
+     int a[100];
+     for (int i=0;i<n;i++)
+     {
+        cin>>a[i];
+     }
+     int m=a[0];
+ for(int i=0;i<n-1;i++)
+ {
+    a[i]=a[i+1];
+ }
+ a[n-1]=m;   
+ for(int i=0;i<n;i++){
+    cout<<a[i];
+ }
+}
